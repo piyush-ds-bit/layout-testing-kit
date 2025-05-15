bee27ba95edb8581af7149ef3cb608422fa05750
piyush-ds-bit <portfoliopiyush8@gmail.com>
2025-05-16 03:13:19 +0530

    Update SocialLinks.tsx

--- src/components/connect/SocialLinks.tsx
+++ src/components/connect/SocialLinks.tsx
@@ -11,7 +11,7 @@ const SocialLinks: React.FC = () => {
   const socialLinks: SocialLink[] = [
     {
       name: 'X',
-      url: 'https://x.com/username',
+      url: 'https://x.com/_piyushkrsingh_',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
           <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
@@ -20,7 +20,7 @@ const SocialLinks: React.FC = () => {
     },
     {
       name: 'Instagram',
-      url: 'https://instagram.com/username',
+      url: 'https://www.instagram.com/_piyushkrsingh_/',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
           <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
@@ -31,7 +31,7 @@ const SocialLinks: React.FC = () => {
     },
     {
       name: 'LinkedIn',
-      url: 'https://linkedin.com/in/username',
+      url: 'https://linkedin.com/in/piyush-singh-55b273305',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
           <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
@@ -42,7 +42,7 @@ const SocialLinks: React.FC = () => {
     },
     {
       name: 'YouTube',
-      url: 'https://youtube.com/username',
+      url: 'https://youtube.com/piyushsingh3984',
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-6 h-6">
           <path d="M2.5 17a24.12 24.12 0 0 1 0-10 2 2 0 0 1 1.4-1.4 49.56 49.56 0 0 1 16.2 0A2 2 0 0 1 21.5 7a24.12 24.12 0 0 1 0 10 2 2 0 0 1-1.4 1.4 49.55 49.55 0 0 1-16.2 0A2 2 0 0 1 2.5 17"></path>
